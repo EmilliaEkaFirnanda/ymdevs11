e92711bd307760eae3e88bfe60b90be6af702553
yahyamahen <54508440+yahyamahen@users.noreply.github.com>
2019-09-17 21:22:15 +0700

    Update Metode_Tabel.c

--- Metode_Tabel.c
+++ Metode_Tabel.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+define maks 100000000
+  
+double f(double x){
+  return (((x*x) - (pow(exp(1),x)))+4);
+}
 
 int main(){
   print("Program Metode Numerik Tabel");
